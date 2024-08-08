ea3bf0a5c9a63b63a50c11c91d0b8d7e16ef828a
Piyush Kumar <piyushkumarsings@gmail.com>
2024-08-09 01:12:19 +0530

    CORS updated again

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
 module.exports = function(app) {
     app.use(
-        '/api',
+        './utils/api',
         createProxyMiddleware({
             target: 'https://cdn.drcode.ai',
             changeOrigin: true,
             pathRewrite: {
-                '^/api': '', // remove /api prefix
+                './utils/api': '',
             },
         })
     );
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,17 @@
-const url = 'https://cdn.drcode.ai/interview-materials/products.json'
+const proxyUrl = 'https://api.allorigins.win/get?url=';
+const url = 'https://cdn.drcode.ai/interview-materials/products.json';
 const options = {
     
     method: 'GET',
     headers: {
-        'Content-Type': 'application/json'
-    },
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers':
+          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': '*',
+        'X-Requested-With': '*',
+      },
   
 };
 try {
@@ -15,7 +22,8 @@ try {
 }
 
 export async function fetchData() {
-    const response = await fetch('/api/interview-materials/products.json');
+    const response = await fetch(proxyUrl + encodeURIComponent(url));
     const result = await response.json();
-    return Object.values(result.products);
+    const products = JSON.parse(result.contents).products;
+    return Object.values(products);
 }

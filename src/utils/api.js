1b6bb9d36e63c16a0cdc59d5c4d61e0bad9c4016
Piyush Kumar <piyushkumarsings@gmail.com>
2024-08-09 00:57:02 +0530

    CORS Policy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,21 @@
-const url='https://cdn.drcode.ai/interview-materials/products.json'
-const options={
-    method:'GET',
+const url = 'https://cdn.drcode.ai/interview-materials/products.json'
+const options = {
+    
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+  
 };
 try {
-    const response =await fetch (url,options);
-    const result= await response.text();
+    const response = await fetch(url, options);
+    const result = await response.text();
 } catch (error) {
     console.log(error);
 }
 
-export async function fetchData(){
-    const response = await fetch('https://cdn.drcode.ai/interview-materials/products.json');
-
+export async function fetchData() {
+    const response = await fetch('/api/interview-materials/products.json');
     const result = await response.json();
-    return Object.values(result.products)
+    return Object.values(result.products);
 }
